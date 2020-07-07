30e4567c81ad914d40cdc641e2189b2db2af2820
Chris Koch <chriskoch@Chriss-Air.home>
2020-07-07 13:53:12 -0400

    Added font-awesome link.

--- src/routes/pip.svelte
+++ src/routes/pip.svelte
@@ -120,6 +120,11 @@
 </style>
 
 <link
+  href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"
+  rel="stylesheet"
+  integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1"
+  crossorigin="anonymous" />
+<link
   src="https://cdnjs.cloudflare.com/ajax/libs/spinkit/2.0.1/spinkit.min.css" />
 
 <main>
