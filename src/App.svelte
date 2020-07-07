5eeb574b033e605767749ce7e2ae0770b66c069a
Chris Koch <chriskoch@Chriss-Air.home>
2020-07-07 13:20:19 -0400

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Router from "svelte-spa-router";
+  import Pip from "./routes/pip.svelte";
+
+  const routes = {
+    "/": Pip
+  };
+</script>
+
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css"
+  rel="stylesheet" />
+
+<main>
+  <Router {routes} />
+</main>
