aa669c28f76f4c06cd8a6e7add419965c0326107
Chris Koch <16182415+chris-koch-penn@users.noreply.github.com>
2023-05-08 18:48:16 -0700

    Updates to code readability for Ruby compile script.

--- wasm/ruby_compile_scripts/main.cpp
+++ wasm/ruby_compile_scripts/main.cpp
@@ -9,18 +9,18 @@
 using namespace std;
 
 extern "C" {
-  int EMSCRIPTEN_KEEPALIVE ruby_exec(int* a, int asize)
+  int EMSCRIPTEN_KEEPALIVE ruby_exec(int* program, int program_size)
   {
-    std::string str;
+    std::string program_string;
 
-    for (int i = 0; i < asize; i++){
-      int value = a[i];
-      str += char(value);
+    for (int i = 0; i < program_size; i++){
+      int value = program[i];
+      program_string += char(value);
     }
 
-    const char* c = str.c_str();
+    const char* program_string_c = program_string.c_str();
     mrb_state *mrb = mrb_open();
-    mrb_value res = mrb_load_string(mrb, c);
+    mrb_value res = mrb_load_string(mrb, program_string_c);
     mrb_close(mrb);
     return 0;
   }
